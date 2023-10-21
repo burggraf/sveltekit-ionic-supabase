d51054db57be23db95382f7863b8db3d0cd1146e
Mark Burggraf <markb@mantisbible.com>
2023-10-21 07:04:46 -0400

    add utility scripts to root

---
+++ gen_types.sh
@@ -0,0 +1,9 @@
+if [ -z "$DB" ];
+then
+  echo "ERROR: DB environment variable not set";
+  echo "run this first:";
+  echo "export DB=https://postgres:password@db.projectref.supabase.co";
+else
+  mkdir -p ./src/interfaces
+  supabase gen types typescript --db-url $DB > ./src/interfaces/schema.ts
+fi
